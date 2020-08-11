48bb097fb856faa48dcfbbf33c07434741534944
Adam Goss <adam.goss94@gmail.com>
2020-08-11 17:17:50 +0200

    Add new views, endpoints and modify panel for logged user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/logged', (req, res) => {
-  res.render('logged');
+router.get('/no-permission', (req, res) => {
+  res.render('noPermission');
+});
+
+const isLogged = (req, res, next) => {
+  if (!req.user) {
+    res.redirect('/user/no-permission');
+  } else {
+    next();
+  }
+};
+
+router.get('/logged', isLogged, (req, res) => {
+  res.render('logged', { name: req.user.displayName, avatar: req.user.photos[0].value });
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/profile', isLogged, (req, res) => {
+  res.render('profile');
+});
 
-module.exports = router; 
+router.get('/profile/settings', isLogged, (req, res) => {
+  res.render('settings');
+});
+
+module.exports = router;
